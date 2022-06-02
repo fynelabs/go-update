16099b9409cb65bc6a62807e1c484bb114bc4d67
Andy Williams <andy@andy.xyz>
2022-06-02 19:40:07 +0100

    Add placeholder for an HTTP source

---
+++ source/http.go
@@ -0,0 +1,12 @@
+package source
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/fynelabs/selfupdate"
+)
+
+func NewHTTPSource(client *http.Client, base *url.URL) selfupdate.Source {
+	panic("not implemented")
+}
